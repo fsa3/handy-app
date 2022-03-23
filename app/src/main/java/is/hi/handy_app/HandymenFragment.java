e950fcaedc8ddecab62445a6867cc9e4d907d3a7
Fannar Steinn <fannarsteinn@hotmail.com>
2022-03-23 11:16:39 +0000

    handyman list

--- app/src/main/java/is/hi/handy_app/HandymenFragment.java
+++ app/src/main/java/is/hi/handy_app/HandymenFragment.java
@@ -1,18 +1,58 @@
 package is.hi.handy_app;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
+import is.hi.handy_app.Entities.HandyUser;
+import is.hi.handy_app.Networking.NetworkCallback;
+import is.hi.handy_app.Networking.NetworkManager;
+
 public class HandymenFragment extends Fragment {
+    private ListView listView;
+    private List<HandyUser> handyUsers;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_handymen, container, false);
+        View view = inflater.inflate(R.layout.fragment_handymen, container, false);
+        listView = (ListView) view.findViewById(R.id.handymen_listview);
+
+        NetworkManager networkManager = NetworkManager.getInstance(getActivity());
+        networkManager.getHandymen(new NetworkCallback<List<HandyUser>>() {
+            @Override
+            public void onSuccess(List<HandyUser> result) {
+                handyUsers = result;
+                ArrayAdapter arrayAdapter = new ArrayAdapter(HandymenFragment.this.getActivity(), android.R.layout.simple_list_item_1, handyUsers);
+                listView.setAdapter(arrayAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(HandymenFragment.this.getActivity() ,handyUsers.get(i).getName(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onaFailure(String errorString) {
+                Log.d(TAG, "Failed to get handymen: " + errorString);
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/is/hi/handy_app/Networking/NetworkManager.java
+++ app/src/main/java/is/hi/handy_app/Networking/NetworkManager.java
@@ -12,7 +12,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.tabs.TabLayout;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -20,6 +19,7 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 import is.hi.handy_app.Entities.Ad;
+import is.hi.handy_app.Entities.HandyUser;
 
 public class NetworkManager {
 
@@ -64,6 +64,27 @@ public class NetworkManager {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d(TAG, "Response: "+error.toString());
+                callback.onaFailure(error.toString());
+            }
+        }
+        );
+        sQueue.add(request);
+    }
+
+    public void getHandymen(NetworkCallback<List<HandyUser>> callback) {
+        StringRequest request = new StringRequest(
+                Request.Method.GET, BASE_URL + "/handymen", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new Gson();
+                Type listType = new TypeToken<List<HandyUser>>() {
+                }.getType();
+                List<HandyUser> handyUsers = gson.fromJson(response, listType);
+                callback.onSuccess(handyUsers);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
                 callback.onaFailure(error.toString());
             }
         }
