da945f91c54aadd6f74a6458c40045781c1db95a
Fannar Steinn Adalsteinsson <fsa3@hi.is>
2022-03-26 18:52:12 +0000

    ad fragment

--- app/src/main/java/is/hi/handy_app/AdFragment.java
+++ app/src/main/java/is/hi/handy_app/AdFragment.java
@@ -1,14 +1,20 @@
 package is.hi.handy_app;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.Base64;
 
 import is.hi.handy_app.Entities.Ad;
 
@@ -16,6 +22,12 @@ public class AdFragment extends Fragment {
     Ad mAd;
 
     TextView mAdTitle;
+    ImageView mAdImage;
+    Button mAdTrade;
+    TextView mAdLocation;
+    TextView mAdDate;
+    TextView mAdDescription;
+    TextView mAdAdvertiser;
 
     public AdFragment(Ad ad) {
         this.mAd = ad;
@@ -27,8 +39,22 @@ public class AdFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ad, container, false);
-        mAdTitle = (TextView) view.findViewById(R.id.ad_title);
+        mAdTitle = view.findViewById(R.id.ad_title);
+        mAdImage = view.findViewById(R.id.ad_image);
+        mAdTrade = view.findViewById(R.id.ad_trade);
+        mAdLocation = view.findViewById(R.id.ad_location);
+        mAdDate = view.findViewById(R.id.ad_date);
+        mAdDescription = view.findViewById(R.id.ad_description);
+        mAdAdvertiser = view.findViewById(R.id.ad_advertiser);
         mAdTitle.setText(mAd.getTitle());
+        byte[] decodedImage = Base64.getDecoder().decode(mAd.getStringImage());
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.length);
+        mAdImage.setImageBitmap(decodedByte);
+        mAdTrade.setText(mAd.getTrade().toString());
+        mAdLocation.setText(mAd.getLocation());
+        mAdDate.setText(mAd.getFormattedDate());
+        mAdDescription.setText(mAd.getDescription());
+        mAdAdvertiser.setText(String.format(getResources().getString(R.string.posted_by), mAd.getUser().getName()));
         return view;
     }
 }
