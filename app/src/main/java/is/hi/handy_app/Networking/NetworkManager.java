f0f44bd7926dab6eed8f4336c8141f4e45349917
Fannar Steinn <fannarsteinn@hotmail.com>
2022-04-20 12:03:59 +0000

    network lint

--- app/src/main/java/is/hi/handy_app/Networking/NetworkManager.java
+++ app/src/main/java/is/hi/handy_app/Networking/NetworkManager.java
@@ -6,26 +6,13 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONObject;
-
-import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import is.hi.handy_app.Entities.Ad;
-import is.hi.handy_app.Entities.HandyUser;
 
 public class NetworkManager {
 
@@ -57,37 +44,19 @@ public class NetworkManager {
     }
 
     public void sendRequest(String url, int method, NetworkCallback<String> callback) {
-        StringRequest request = new StringRequest(method, BASE_URL + url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                callback.onSuccess(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d(TAG, error.toString());
-                callback.onaFailure(error.toString());
-            }
+        StringRequest request = new StringRequest(method, BASE_URL + url, callback::onSuccess, error -> {
+            Log.d(TAG, error.toString());
+            callback.onaFailure(error.toString());
         });
         sQueue.add(request);
     }
 
     public void sendRequestWithBody(String url, int method, JSONObject body, NetworkCallback<String> callback) {
         JsonObjectRequest request = new JsonObjectRequest(method, BASE_URL + url, body,
-                new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        callback.onSuccess(response.toString());
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d("JSONPost", "Error: " + error.getMessage());
-                callback.onaFailure(error.getMessage());
-            }
-        });
+                response -> callback.onSuccess(response.toString()), error -> {
+                    VolleyLog.d("JSONPost", "Error: " + error.getMessage());
+                    callback.onaFailure(error.getMessage());
+                });
         sQueue.add(request);
     }
 }
